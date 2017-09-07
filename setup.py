d8766a1bf841a1205b3151806ee22fa5407aaf16
Felipe Santiago <fsantiag@thoughtworks.com>
2017-09-07 17:02:29 -0300

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='HelloWorld',
+      description='A simple application to apply DI concepts',
+      author='Felipe Santiago',
+      author_email='fsantiag@thoughtworks.comi',
+      version='1.0',
+      py_modules=['hello'],
+)
