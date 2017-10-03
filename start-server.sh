a12164c275b0e4f291a8575540995a589b1a9d06
root <root@ip-172-31-11-52.us-east-2.compute.internal>
2017-10-03 01:34:33 +0000

    Adjust startup script:wq

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cd /home/fsantiag/flask-ap
-tox
-source .tox/py35/bin/activate
+cd /home/fsantiag/flask-app
 export FLASK_APP=iphone8.py
-python3 -m flask run --host=0.0.0.0 --port=54321
+nohup python3 -m flask run --host=0.0.0.0 --port=54321 &
