9afcc86f908eb513a0c48e76c819f0c54f6cf0fc
Felipe Santiago <fsantiag@thoughtworks.com>
2017-10-22 03:26:45 -0200

    Remove nohup from startup script

--- start-server.sh
+++ start-server.sh
@@ -3,4 +3,4 @@ export FLASK_APP=iphone8.py
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-nohup python3 -m flask run --host=0.0.0.0 --port=54321 > /home/fsantiag/log.txt 2>&1 &
+python3 -m flask run --host=0.0.0.0 --port=54321
