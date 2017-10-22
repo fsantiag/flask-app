953849f3dc2c580774874860e7ce6fe6d0358fdd
Felipe Santiago <fsantiag@thoughtworks.com>
2017-10-22 00:15:23 -0200

    Change the start server script

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 export FLASK_APP=iphone8.py
-flask run --host=0.0.0.0 --port=54321
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+nohup python3 -m flask run --host=0.0.0.0 --port=54321 > /home/fsantiag/log.txt 2>&1 &
